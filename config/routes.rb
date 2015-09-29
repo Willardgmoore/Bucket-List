ff290ee037479ccae6a7800e7ee3722736c8a780
Scotchard22 <scotchard@aol.com>
2015-09-28 19:52:41 -0600

    profile route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :users
 
+  get '/profile', to: 'users#profile', as: :profile
 
 
   # The priority is based upon order of creation: first created -> highest priority.
