f35a7ba11645ae17661728ef687a2c803e1d7f61
Will <will@thefixitgeek.com>
2015-09-29 15:50:49 -0600

    update activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,54 @@
 class ActivitiesController < ApplicationController
   def index
+  	@user = activities.all
   end
 
   def show
+  	@activity = activity.find(params[:id])
   end
 
   def edit
+  	@activity = activity.find(params[:id])
   end
 
-  def new
+  def create
+  	@activity = activity.new(user_params)
+
+	  if @activity.save
+	    redirect_to @activity
+	  else
+	    render 'new'
+	  end
+	end
+
+	def new
+		@activity = activity.new
+	end
+
+	def update
+	  @activity = activity.find(params[:id])
+ 
+	  if @activity.update(article_params)
+	    redirect_to @activity
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+		@activity = activity.find(params[:id])
+		@activity.destroy method: :delete
+
+		redirect_to users_path
+	end
+
+	def profile
+	end
+
+  private
+
+  def user_params
+  	params.require(:activity).permit(:first_name, :last_name, :description)
   end
 end
+
