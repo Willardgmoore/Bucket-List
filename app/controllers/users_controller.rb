39d40bd9d624d1225b7feb9d68fccf3c3a32e162
Will <will@thefixitgeek.com>
2015-09-29 15:17:04 -0600

    update views and 1 controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,48 @@
 class UsersController < ApplicationController
   def index
+  	@user = User.new(user_params)
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+
+	  if @user.save
+	    redirect_to @user
+	  else
+	    render 'new'
+	  end
+	end
+
+	def new
+	end
+
+	def update
+  @user = User.find(params[:id])
+ 
+	  if @user.update(article_params)
+	    redirect_to @user
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	end
+
+	def profile
+	end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :description)
   end
 end
